719c96fb9dd5b1929b13247daa7b985cf0d87f41
MrGodfrey <562568681@qq.com>
2019-04-21 23:06:51 +0800

    修改分数反馈

--- miniprogram/pages/scoreBoard/scoreBoard.js
+++ miniprogram/pages/scoreBoard/scoreBoard.js
@@ -15,7 +15,7 @@ Page({
     inputValue: "",
   },
 
-  onLoad: function () {
+  onLoad: function() {
     var res = wx.getStorageSync('namelist')
     var len = res.length
     for (var i = 0; i < len; i++) {
@@ -29,18 +29,18 @@ Page({
     })
   },
 
-  onHide: function () {
+  onHide: function() {
     this.storge()
   },
 
-  storge: function () {
+  storge: function() {
     wx.setStorage({
       key: 'namelist',
       data: this.data.namelist,
     })
   },
 
-  optionTap: function (e) {
+  optionTap: function(e) {
     if (this.data.optionHide === "None") {
       this.setData({
         optionHide: "inline",
@@ -52,11 +52,11 @@ Page({
     }
   },
 
-  onUnload: function () {
+  onUnload: function() {
     this.storge()
   },
 
-  deletePlayer: function (e) {
+  deletePlayer: function(e) {
     var index = e.target.dataset.player
     this.data.namelist.splice(index, 1)
     this.setData({
@@ -65,30 +65,44 @@ Page({
     this.storge()
   },
 
-  sliderChange: function (e) {
+  sliderChange: function(e) {
     this.setData({
       addValue: e.detail.value,
     })
   },
 
-  addScore: function (e) {
+  addScore: function(e) {
     var index = e.target.dataset.player
     this.data.namelist[index].score += this.data.addValue
     this.setData({
       namelist: this.data.namelist
     })
+    var that=this
+    wx.showToast({
+      title: that.data.namelist[index].name + '  +' + that.data.addValue,
+      icon: 'success',
+      duration: 500,
+      mask: true
+    });
     this.storge()
   },
-  minusScore: function (e) {
+  minusScore: function(e) {
     var index = e.target.dataset.player
     this.data.namelist[index].score -= this.data.addValue
     this.setData({
       namelist: this.data.namelist
     })
+    var that = this
+    wx.showToast({
+      title: that.data.namelist[index].name + '  -' + that.data.addValue,
+      icon: 'success',
+      duration: 500,
+      mask: true
+    });
     this.storge()
   },
 
-  nameinput: function (e) {
+  nameinput: function(e) {
     console.log(e)
     var newplayer = {}
     newplayer.name = e.detail.value
@@ -103,7 +117,7 @@ Page({
 
 
 
-  deleteChangeStyle: function (e) {
+  deleteChangeStyle: function(e) {
     if (this.data.deleteState === "None") {
       console.log(e)
       this.setData({
